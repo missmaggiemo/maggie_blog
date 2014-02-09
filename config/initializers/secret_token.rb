d7555afcc409c344c2b47d262af28761e5fddc2c
margaret.m.moreno@gmail.com <margaret.m.moreno@gmail.com>
2014-02-08 18:41:17 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MaggieBlog::Application.config.secret_key_base = 'af2a65933a228f5736fd3ed7ac2400fb1870fc2bd4aee6750849f4fd731e234b19d54bbc78566fe4c6cbed4a336325f389364a5a0197a07cfea00685944f8f8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MaggieBlog::Application.config.session_store :cookie_store, key: '_maggie_blog_session'
